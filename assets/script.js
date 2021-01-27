42e7c44eadf838197da47f6376a85449cfaaec69
Esteban Mendez <edmendezn@gmail.com>
2021-01-27 11:41:22 -0500

    Added playButton eventlistener and function. Lines 2,3,4,16,and 193 of script file

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
 //-------------------------------------vArs----------------------
+var playButton = $("#play-button");
+var welcomePage = $(".welcome-container");
+var displayMainPage = $("#main-page");
 var Buttonfunction;
 var int;
 var movie = "titanic";
@@ -10,23 +13,29 @@ var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=8e35679c";
 var genre;
 
 //-------------------------CLICK/UNCLICK----------------------------
-var option = new Array(
+function startMovieSearch() {
+  // The Welcome page will hide
+  welcomePage.addClass("hide");
+  // The movie search page will be displayed
+  displayMainPage.removeClass("hide");
+
+  var option = new Array(
     document.getElementById("#gnere1").addEventListener("click", one),
     document.getElementById("#gnere2").addEventListener("click", two),
     document.getElementById("#gnere3").addEventListener("click", three),
     document.getElementById("#gnere4").addEventListener("click", four),
     document.getElementById("#gnere5").addEventListener("click", five),
     document.getElementById("#gnere6").addEventListener("click", six)
-    );
-    var optionb = new Array(
-        document.getElementById("#gnere1"),
-        document.getElementById("#gnere2"),
-        document.getElementById("#gnere3"),
-        document.getElementById("#gnere4"),
-        document.getElementById("#gnere5"),
-        document.getElementById("#gnere6")
-        );
-function one(){
+  );
+  var optionb = new Array(
+    document.getElementById("#gnere1"),
+    document.getElementById("#gnere2"),
+    document.getElementById("#gnere3"),
+    document.getElementById("#gnere4"),
+    document.getElementById("#gnere5"),
+    document.getElementById("#gnere6")
+  );
+  function one() {
     optionb[1].checked = false;
     optionb[2].checked = false;
     optionb[3].checked = false;
@@ -34,8 +43,8 @@ function one(){
     optionb[5].checked = false;
     genre = 1;
     display();
-}
-function two(){
+  }
+  function two() {
     optionb[0].checked = false;
     optionb[2].checked = false;
     optionb[3].checked = false;
@@ -43,8 +52,8 @@ function two(){
     optionb[5].checked = false;
     genre = 2;
     display();
-}
-function three(){
+  }
+  function three() {
     optionb[1].checked = false;
     optionb[0].checked = false;
     optionb[3].checked = false;
@@ -52,8 +61,8 @@ function three(){
     optionb[5].checked = false;
     genre = 3;
     display();
-}
-function four(){
+  }
+  function four() {
     optionb[1].checked = false;
     optionb[2].checked = false;
     optionb[0].checked = false;
@@ -61,8 +70,8 @@ function four(){
     optionb[5].checked = false;
     genre = 4;
     display();
-}
-function five(){
+  }
+  function five() {
     optionb[1].checked = false;
     optionb[2].checked = false;
     optionb[3].checked = false;
@@ -70,8 +79,8 @@ function five(){
     optionb[5].checked = false;
     genre = 5;
     display();
-}
-function six(){
+  }
+  function six() {
     optionb[1].checked = false;
     optionb[2].checked = false;
     optionb[3].checked = false;
@@ -79,101 +88,106 @@ function six(){
     optionb[0].checked = false;
     genre = 6;
     display();
-}
+  }
 
-//---------------------------------------DISPLAY MOVIES------------------------
-function display(){
-    if(genre === 6){
-        //add and remove search option
-        var html = '<input id="id-movieinput" type="text" placeholder="Search movie"/> <input id="id-searchBtn" type="button" value="search"/>';
-        document.getElementById("id-searchdiv").innerHTML = html;
-        Buttonfunction = document.getElementById("id-searchBtn").addEventListener("click", getmovie);
-        console.log(genre);
+  //---------------------------------------DISPLAY MOVIES------------------------
+  function display() {
+    if (genre === 6) {
+      //add and remove search option
+      var html =
+        '<input id="id-movieinput" type="text" placeholder="Search movie"/> <input id="id-searchBtn" type="button" value="search"/>';
+      document.getElementById("id-searchdiv").innerHTML = html;
+      Buttonfunction = document
+        .getElementById("id-searchBtn")
+        .addEventListener("click", getmovie);
+      console.log(genre);
 
-        //other code eventually
-
+      //other code eventually
     }
-    if(genre === 5){
-        //add and remove search option
-        var html = '';
-        document.getElementById("id-searchdiv").innerHTML = html;
-        console.log(genre);
+    if (genre === 5) {
+      //add and remove search option
+      var html = "";
+      document.getElementById("id-searchdiv").innerHTML = html;
+      console.log(genre);
 
-        //other code eventually
-        
+      //other code eventually
     }
-    if(genre === 4){
-            //add and remove search option
-            var html = '';
-            document.getElementById("id-searchdiv").innerHTML = html;
-            console.log(genre);
-    
-            //other code eventually
-            
+    if (genre === 4) {
+      //add and remove search option
+      var html = "";
+      document.getElementById("id-searchdiv").innerHTML = html;
+      console.log(genre);
+
+      //other code eventually
     }
-    if(genre === 3){
-             //add and remove search option
-             var html = '';
-             document.getElementById("id-searchdiv").innerHTML = html;
-             console.log(genre);
-     
-             //other code eventually
-                
+    if (genre === 3) {
+      //add and remove search option
+      var html = "";
+      document.getElementById("id-searchdiv").innerHTML = html;
+      console.log(genre);
+
+      //other code eventually
     }
-    if(genre === 2){
-             //add and remove search option
-             var html = '';
-             document.getElementById("id-searchdiv").innerHTML = html;
-             console.log(genre);
-     
-             //other code eventually
-                
+    if (genre === 2) {
+      //add and remove search option
+      var html = "";
+      document.getElementById("id-searchdiv").innerHTML = html;
+      console.log(genre);
+
+      //other code eventually
     }
-    if(genre === 1){
-              //add and remove search option
-              var html = '';
-              document.getElementById("id-searchdiv").innerHTML = html;
-              console.log(genre);
-      
-              //other code eventually
-               
+    if (genre === 1) {
+      //add and remove search option
+      var html = "";
+      document.getElementById("id-searchdiv").innerHTML = html;
+      console.log(genre);
+
+      //other code eventually
     }
-}
+  }
 
-
-
-
-
-//-----------------GET MOVIE INFO----------------------
-function getmovie(){
+  //-----------------GET MOVIE INFO----------------------
+  function getmovie() {
     movie = document.getElementById("id-movieinput").value;
-    queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=8e35679c"
+    queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=8e35679c";
     getmovieinfo();
-}
-function getmovieinfo(){
+  }
+  function getmovieinfo() {
     $.ajax({
-        type: "GET", 
-        url: queryURL,
-        dataType: "json",
-        success: function(data){
-            console.log(data);
+      type: "GET",
+      url: queryURL,
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
 
-            console.log(data.Title);
-            title = data.Title;
-            console.log(data.Year);
-            year = data.Year;
-            console.log(data.Poster);
-            poster = data.Poster;
-            console.log(data.Plot);
-            plot = data.Plot;
-            htmledit();
-        }
+        console.log(data.Title);
+        title = data.Title;
+        console.log(data.Year);
+        year = data.Year;
+        console.log(data.Poster);
+        poster = data.Poster;
+        console.log(data.Plot);
+        plot = data.Plot;
+        htmledit();
+      },
     });
-    }
-function htmledit(){
-    var html = '<h3> ' + title + '<br> ' + year + '</h3> <p1><img src = "'+ poster + '" style = "width:200px;height:300px;"><br>' + plot +  '</p1>'
+  }
+  function htmledit() {
+    var html =
+      "<h3> " +
+      title +
+      "<br> " +
+      year +
+      '</h3> <p1><img src = "' +
+      poster +
+      '" style = "width:200px;height:300px;"><br>' +
+      plot +
+      "</p1>";
     document.getElementById("id-movieName").innerHTML = html;
+  }
+  getmovieinfo();
 }
-getmovieinfo();
 
-
+// When the user clicks on the playButton then the
+// Movie Search Page will be displayed.
+$(playButton).on("click", startMovieSearch);
